1d6217bcb8c3f7f112d76e9013ec67d10cac8114
Adam Krone <krone.adam@gmail.com>
2014-03-28 11:29:26 -0500

    Adds basic viewer tests

---
+++ spec/viewer_spec.rb
@@ -0,0 +1,66 @@
+require 'webshot'
+
+describe Webshot::Viewer do
+  let(:viewer) { Webshot::Viewer.new }
+
+  describe "#versions" do
+    it "should return an array of versions" do
+      versions = viewer.versions
+
+      expect(versions).to be_kind_of(Array)
+    end
+  end
+
+  describe "#current_version" do
+    it "should return the current version" do
+      pending "Need to setup a default set of testing screenshots"
+    end
+  end
+
+  describe "#previous_version" do
+    it "should return the previous version" do
+      pending "Need to setup a default set of testing screenshots"
+    end
+  end
+
+  describe "#browsers" do
+    it "should return an array of browsers" do
+      browsers = viewer.browsers
+
+      expect(browsers).to be_kind_of(Array)
+    end
+  end
+
+  describe "#breakpoints" do
+    it "should return an array of breakpoints" do
+      breakpoints = viewer.breakpoints
+
+      expect(breakpoints).to be_kind_of(Array)
+    end
+  end
+
+  describe "#pages" do
+    it "should return an array of pages" do
+      pages = viewer.pages
+
+      expect(pages).to be_kind_of(Array)
+    end
+  end
+
+  describe "#screenshots" do
+    it "should return an array of screenshots" do
+      screenshots = viewer.screenshots
+
+      expect(screenshots).to be_kind_of(Array)
+    end
+  end
+
+
+  describe "#diffs" do
+    it "should return an array of diffs" do
+      diffs = viewer.diffs
+
+      expect(diffs).to be_kind_of(Array)
+    end
+  end
+end
